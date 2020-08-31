b9348ba8d85d596a934be51c4e177669508b0ef5
Viet Nguyen <vnguy143@calpoly.edu>
2020-08-30 22:01:08 -0700

    Set to best config, MAE~24

--- run_training.sh
+++ run_training.sh
@@ -10,5 +10,10 @@ python3 train.py \
     --val-start 0 \
     --save-dir ./outdir \
     --crop-size 256 \
-    --batch-size 32 \
-    --num-workers 1
+    --batch-size 1 \
+    --num-workers 1 \
+    --lr 1e-6 \
+    --weight-decay 1e-4 \
+    --downsample-ratio 8 \
+    --sigma 8.0 \
+    --background-ratio 0.15
